3852d45a798fd7f3770446fef716de4efa617b73
tonyzheng6 <zheng.tony6@gmail.com>
2015-01-31 16:52:57 -0500

    Update Community.java

--- src/Community.java
+++ src/Community.java
@@ -1,3 +1,15 @@
+/*******************************************************************************************************
+ * Title:			Community.java
+ * Author:			Tony Zheng, Andrew Baumann, Grace Liang, Katherine Sullivan 
+ * Special thanks: 	Devon
+ * Created On:		January 31, 2015
+ * Description:		A small scale pseudo community forum where users can post suggestions about 
+ * 					improving the community and getting people more involved by posting comments on how 
+ * 					they can achieve it.
+ * Build with:		Eclipse or using the following commands
+ * 					To compile: javac Community.java Forum.java Suggestions.java
+ * 					To run: 	java Community.java
+ *******************************************************************************************************/
 import javax.swing.JFrame;
 
 public class Community 
@@ -6,7 +18,7 @@ public class Community
 	{
 		Forum myForum = new Forum();
 		myForum.setTitle("Community Forum");
-		myForum.setSize(1000, 1000);
+		myForum.setSize(750, 275);
 		myForum.setLocationRelativeTo(null);
 		myForum.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myForum.setVisible(true);
